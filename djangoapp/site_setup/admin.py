70ca76d2c37ff6c20dbe41a61e01539ebf8cbd5a
Ryan Santana <ryan.santana.leal@outlook.com>
2025-02-03 10:25:13 -0300

    Usando Context Processors para injetar valores em todos os templates do Django

--- djangoapp/site_setup/admin.py
+++ djangoapp/site_setup/admin.py
@@ -1,17 +1,22 @@
 from django.contrib import admin
 from site_setup.models import MenuLink, SiteSetup
 
+# @admin.register(MenuLink)
+# class MenuLinkAdmin(admin.ModelAdmin):
+#     list_display = 'id', 'text', 'url_or_path',
+#     list_display_links = 'id', 'text', 'url_or_path',
+#     search_fields = 'id', 'text', 'url_or_path',
 
-@admin.register(MenuLink)
-class MenuLinkAdmin(admin.ModelAdmin):
-    list_display = 'id', 'text', 'url_or_path',
-    list_display_links = 'id', 'text', 'url_or_path',
-    search_fields = 'id', 'text', 'url_or_path',
+
+class MenuLinkInline(admin.TabularInline):
+    model = MenuLink
+    extra = 1
 
 
 @admin.register(SiteSetup)
 class SiteSetupAdmin(admin.ModelAdmin):
     list_display = 'title', 'description',
+    inlines = MenuLinkInline,
 
     # Revoga a permissão de insert caso já tenha um valor na tabela
     # (apenas na área administrativa)
