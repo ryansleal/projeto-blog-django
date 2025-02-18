14e5d79d53e1a88d4713bcc4816ded13eaab997a
Ryan Santana <ryan.santana.leal@outlook.com>
2025-02-18 09:01:01 -0300

    get_absolute_url + criando um campo readonly na admin do Django

--- djangoapp/blog/admin.py
+++ djangoapp/blog/admin.py
@@ -1,5 +1,6 @@
 from blog.models import Category, Page, Post, Tag
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from django_summernote.admin import SummernoteModelAdmin  # type: ignore
 
 
@@ -52,11 +53,24 @@ class PostAdmin(SummernoteModelAdmin):
     list_filter = 'category', 'is_published',
     list_editable = 'is_published',
     ordering = '-id',
-    readonly_fields = 'created_at', 'updated_at', 'created_by', 'updated_by',
+    readonly_fields = (
+        'created_at', 'updated_at', 'created_by', 'updated_by', 'link',
+    )
     prepopulated_fields = {
         'slug': ('title',),
     }
     autocomplete_fields = 'tags', 'category',
+
+    def link(self, obj):
+        if not obj.pk:
+            return '-'
+
+        post_url = obj.get_absolute_url()
+        safe_link = mark_safe(
+            f'<a target="_blank" href="{post_url}">Ver post</a>'
+        )
+
+        return safe_link
 
     def save_model(self, request, obj, form, change):
         if change:
--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
 from django_summernote.models import AbstractAttachment  # type: ignore
 from utils.images import resize_image
 from utils.rands import slugify_new
@@ -135,6 +136,11 @@ class Post(models.Model):
     tags: models.ManyToManyField = models.ManyToManyField(
         Tag, blank=True, default=''
     )
+
+    def get_absolute_url(self):
+        if not self.is_published:
+            return reverse('blog:index')
+        return reverse('blog:post', args=(self.slug,))
 
     def save(self, *args, **kwargs):
         if not self.slug:
